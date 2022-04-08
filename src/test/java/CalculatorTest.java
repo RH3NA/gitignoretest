dd6316c82a75317b14b22eeaba1f10ce3b514b60
Rebecca <rebfje-1@student.ltu.se>
2022-04-08 06:44:27 +0200

    testing

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+    class CalculatorTest {
+
+        @Test
+        @DisplayName("2+1=3")
+        void addsTwoNumbers() {
+            Calculator calculator = new Calculator();
+            assertEquals(3, calculator.add(2,1), "2+1 should be 3");
+        }
+
+    }
